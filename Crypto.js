c417ab38c13437c727831de503bcc246bf7b4f2a
oyanezm <oyanezm@gmail.com>
2013-10-17 15:34:06 -0300

    added first version of Crypto

---
+++ Crypto.js
@@ -0,0 +1,155 @@
+
+// Set Globals
+var Alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+var Key = ''; // TODO: get this from config
+
+
+/**
+ * @method get_EncryptedString()
+ *
+ * Encription Function
+ * Based on a Encription Key.
+ * The Encryption Key is a set of number betwen 0-6.
+ * E.g  '12' VALID!
+ *      '123sdd0' INVALID!
+ *
+ * The encryption method consist in three steps:
+ *
+ * 1. First Step: depends on the type of character.
+ *
+ * Each letter of the string will be encrypted based on each
+ * number of the Encryption Key.
+ *
+ * a. if the character is Alphabetic thus a-zA-Z
+ *    the encryption will be the next X letters in the alphabetic.
+ *    where X comes from Encryption Key.
+ *    E.g 'hoLA' encrypted by '1' is 'ipMB'
+ *        'hoLA' encrypted by '12' is 'ijpqMNBC
+ *
+ *    in we pass the alphabet limit then loop in the alphabet
+ *    E.g 'xYz' encrypted by '3' is 'aBc'
+ *        '
+ * b. if the character is a number 0-9 then the encryption
+ *    will be the Numer + X.
+ *    E.g '0123' encrypted by '1' is '1234'
+ *        '0123' encrypted by '12' is '12233445'
+ *
+ *    if we pass the 0-9 limit then we loop
+ *    E.g '89' encrypted by '2' is '01'
+ *
+ * c. if the character is non numeric and non alphabetic
+ *    then no operation is apply rather than duplicate the value.
+ *    E.g '_$#' encrypted by '1' is '_$#' (the same)
+ *        '_$#' encrypted by '15' is '__$$##' (the same)
+ *
+ * 2. Second Step: reverse the string. E.g 'ipLA' => 'ALpi'
+ *
+ * 3. Third Step: swap letters.
+ *  a. Swap First and Last Letter. E.g 'hola' => 'aolh'
+ *  b. Swap (First + 3) by (Last -3) E.g 'abcDEfgh' => 'abceEDfgh'
+ *
+ * Tests
+ *
+ * ENCRYPTION CODE: '152'
+ * '123456'       => '217306695584477368'
+ * 'hello_world'  => 'iiefqmtwsqtpybx___qtpnqmnqmgjnjmf'
+ * 'hola&chao12'  => 'i73p62qtpcfbjmiehd&&&cfbnqmqt3jm4'
+ *
+ * ENCRYPTION CODE: '523'
+ * '123456'       => '681770769658548439'
+ * 'hello_world'  => 'mfijnqutwrqtzyb___rqtonqonqhgokjg'
+ * 'hola&chao12'  => 'm47t36rqtdcfkjmfeh&&&dcfonqrq4kj5'
+ * 'lalala'       => 'qcffnqdcfonqdcoond'
+ *
+ **/
+function get_EncryptedString(string){
+
+    // encode each character
+    encrypted = "";
+    for(var i = 0; i < string.length; i++){
+        chr = string.charAt(i);
+        encrypted = encrypted.concat(get_EncryptedChar(chr));
+    };
+
+    // reverse string
+    encrypted = encrypted.split("").reverse().join("");
+
+    // swap letters TODO: make function of this
+    encrypted = swap_letters(encrypted,new Array(3,3));
+    encrypted = swap_letters(encrypted,new Array(0,0));
+
+    return encrypted;
+};
+
+/** swap letter sin a string based on position **/
+function swap_letters(string,positions){
+    string_list = string.split("");
+
+    oldchar = string_list[positions[0]];
+    newchar = string_list[string.length - 1 - positions[1]];
+
+    string_list[positions[0]] = newchar;
+    string_list[string.length - 1 - positions[1]] = oldchar;
+
+    return string_list.join("");
+}
+
+/** encrypts a sling character **/
+function get_EncryptedChar(chr){
+    if (is_Alphabet(chr)){ return get_EncryptedLetter(chr);
+    }else if(is_Numeric(chr)){ return get_EncryptedNumber(chr);}
+
+    // is non alphanumeric then just repeat
+    string = ''
+    for(var i = 0; i < Key.length; i++){
+        string = string.concat(chr);
+    }
+    return string;
+}
+
+/** encrypts a letter **/
+function get_EncryptedLetter(letter){
+    string = '';
+    is_Upper = (letter == letter.toUpperCase());
+
+    for(var i = 0; i < Key.length; i++){
+        offset = parseInt(Key.charAt(i));
+        letter_pos = get_AlphabetPosition(letter);
+        encrypted_pos = letter_pos + offset;
+        if(encrypted_pos > 25){ encrypted_pos = encrypted_pos - 26; }
+        string = string.concat(Alphabet[encrypted_pos]);
+    }
+
+    if(is_Upper) return string.toUpperCase();
+    return string;
+}
+
+/** gets position in alphabet **/
+function get_AlphabetPosition(letter){
+    for( i = 0; i < Alphabet.length; i++){
+        if (letter.toLowerCase() == Alphabet[i]){ return i;}
+    }
+    return 0;
+}
+
+/** encrypts a number **/
+function get_EncryptedNumber(num){
+    string  = '';
+    for(var i = 0; i < Key.length; i++){
+        offset = Key.charAt(i);
+        encrypted = String(parseInt(num) + parseInt(offset));
+
+        // if over 10 get nominal part
+        if (parseInt(encrypted) > 9){
+            encrypted = encrypted.charAt(1);
+        }
+        string = string.concat(encrypted);
+    }
+    return string;
+}
+
+/** validates character ir alphabet **/
+function is_Alphabet(letter){return /^[a-zA-Z]$/.test(letter);}
+
+/** validates numeric character **/
+function is_Numeric(num){return /^[0-9]$/.test(num);}
---
+++ Crypto.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * Encription Class
+ * The encryption method is based on a Encription Key.
+ * The Encryption Key is a set of number betwen 0-6.
+ * E.g  '12' VALID!
+ *      '123sdd0' INVALID!
+ *
+ * The encryption method consist in three steps:
+ *
+ * 1. First Step: depends on the type of character.
+ *
+ * Each letter of the string will be encrypted based on each
+ * number of the Encryption Key.
+ *
+ * a. if the character is Alphabetic thus a-zA-Z
+ *    the encryption will be the next X letters in the alphabetic.
+ *    where X comes from Encryption Key.
+ *    E.g 'hoLA' encrypted by '1' is 'ipMB'
+ *        'hoLA' encrypted by '12' is 'ijpqMNBC
+ *
+ *    in we pass the alphabet limit then loop in the alphabet
+ *    E.g 'xYz' encrypted by '3' is 'aBc'
+ *        '
+ * b. if the character is a number 0-9 then the encryption
+ *    will be the Numer + X.
+ *    E.g '0123' encrypted by '1' is '1234'
+ *        '0123' encrypted by '12' is '12233445'
+ *
+ *    if we pass the 0-9 limit then we loop
+ *    E.g '89' encrypted by '2' is '01'
+ *
+ * c. if the character is non numeric and non alphabetic
+ *    then no operation is apply rather than duplicate the value.
+ *    E.g '_$#' encrypted by '1' is '_$#' (the same)
+ *        '_$#' encrypted by '15' is '__$$##' (the same)
+ *
+ * 2. Second Step: reverse the string. E.g 'ipLA' => 'ALpi'
+ *
+ * 3. Third Step: swap letters.
+ *  a. Swap First and Last Letter. E.g 'hola' => 'aolh'
+ *  b. Swap (First + 3) by (Last -3) E.g 'abcDEfgh' => 'abceEDfgh'
+ *
+ * Tests
+ *
+ * ENCRYPTION CODE: '152'
+ * '123456'       => '217306695584477368'
+ * 'hello_world'  => 'iiefqmtwsqtpybx___qtpnqmnqmgjnjmf'
+ * 'hola&chao12'  => 'i73p62qtpcfbjmiehd&&&cfbnqmqt3jm4'
+ *
+ * ENCRYPTION CODE: '523'
+ * '123456'       => '681770769658548439'
+ * 'hello_world'  => 'mfijnqutwrqtzyb___rqtonqonqhgokjg'
+ * 'hola&chao12'  => 'm47t36rqtdcfkjmfeh&&&dcfonqrq4kj5'
+ * 'lalala'       => 'qcffnqdcfonqdcoond'
+ *
+ **/
+class Crypto{
+
+  /** encrypts a string base don a key **/
+  static function encrypt($string,$key){
+    $length = strlen($string);
+    $encrypted = '';
+
+    for($i = 0; $i<$length; $i++){
+      $enc_char = $char = $string[$i];
+      if (ctype_alpha($char))
+        $enc_char = self::encrypt_char($char,$key);
+      elseif (is_numeric($char))
+        $enc_char = self::encrypt_num($char,$key);
+      else
+        $enc_char = self::encrypt_odd($char,$key);
+      $encrypted .= $enc_char;
+    }
+
+    // reverse
+    $encrypted = strrev($encrypted);
+
+    // swap characters
+    $encrypted = self::swap_characters($encrypted,array(0,3));
+
+    return $encrypted;
+  }
+
+  /** swap character positions **/
+  static function swap_characters($string,$positions){
+    $length = strlen($string) - 1;
+    foreach($positions as $pos){
+      $char1 = $string[$pos];
+      $char2 = $string[$length - $pos];
+      $string[$pos] = $char2;
+      $string[$length - $pos] = $char1;
+    }
+    return $string;
+  }
+
+  /** decrypts a string based on a key **/
+  static function decrypt($string,$key){
+
+    $string = self::swap_characters($string,array(0,3));
+    $string = strrev($string);
+    $length = strlen($key);
+    $length_str = strlen($string);
+
+    $decrypted = '';
+    $i = 0;
+
+    // decrypt based on the first key
+    while($i<$length_str){
+      $enc_char = $char = $string[$i];
+
+      // map character
+      if(ctype_alpha($char)){
+        $enc_char = chr(ord($enc_char) - (int) $key[0]);
+        $enc_char = self::decrypt_char($enc_char);
+      }
+      if(is_numeric($char))
+        $enc_char = self::decrypt_num($enc_char, (int) $key[0]);
+
+      $decrypted .= $enc_char;
+      $i = $i + $length;
+    }
+
+    return $decrypted;
+  }
+
+  /** decrypts an alphabetic character **/
+  static function decrypt_char($char){
+    $translate = array(
+      // Mayus
+      ':' => 'T',     ';' => 'U',   '<' => 'V',   '=' => 'W',
+      '>' => 'X',     '?' => 'Y',   '@' => 'Z',
+      // Minus
+      '[' => 'u',     '\\' => 'v', ']' => 'w',   '^' => 'x',
+      '_' => 'y',     '`' => 'z'
+    );
+    if (@$translate[$char]) return $translate[$char];
+    return $char;
+  }
+
+  /** decrypts a number **/
+  static function decrypt_num($num,$key){
+    if($num-$key < 0) return $num - $key +10;
+    return $num-$key;
+  }
+
+  /** encrypts a non alpha and non numeric char **/
+  static function encrypt_odd($odd,$key){
+    $length = strlen($key);
+    $encrypt = '';
+    for($i = 0; $i < $length; $i++) $encrypt .= $odd;
+    return $encrypt;
+  }
+
+  /** encrypts a number **/
+  static function encrypt_num($num,$key){
+    $length = strlen($key);
+    $encrypt = '';
+    for($i = 0; $i < $length; $i++){
+      $enc_num = $num + $key[$i];
+      if($enc_num >= 10){
+        $enc_num = (string) $enc_num;
+        $enc_num = $enc_num[1];
+      }
+      $encrypt .= $enc_num;
+    }
+    return $encrypt;
+  }
+
+  /** encrypts an alphabet character **/
+  static function encrypt_char($char,$key){
+    $length = strlen($key);
+    $encrypted = '';
+
+    for($i = 0; $i<$length; $i++){
+      // set letter
+      $enc_char = $char;
+
+      if (ctype_alpha($enc_char)){
+        for($j = 0; $j<$key[$i]; $j++) ++$enc_char;
+        $enc_char = (strlen($enc_char)>1)? $enc_char[1]: $enc_char;
+      }
+
+      $encrypted .= $enc_char;
+    }
+    return $encrypted;
+  }
+}
+?>
